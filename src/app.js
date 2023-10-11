27acfc1a4c2fd4a2c4bef9ff7b7d46dd5b025b1c
LucaPinheiro <lucapgomes11@gmail.com>
2023-10-11 17:12:08 -0300

    adicionando middleware no código

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import conectaNaDatabase from "./config/dbConnect.js";
 import routes from "./routes/index.js";
+import mongoose from "mongoose";
 
 const conexao = await conectaNaDatabase();
 
@@ -12,5 +13,16 @@ conexao.once("open", () => {
 const app = express();
 app.use(express.json());
 routes(app);
+
+// eslint-disable-next-line no-unused-vars
+app.use((erro, req, res, next) => {
+  if (erro instanceof mongoose.Error.CastError) {
+    res
+      .status(400)
+      .send({ message: "Um ou mais dados fornecidos estão incorretos." });
+  } else {
+    res.status(500).send({ message: "Erro inteino de servidor" });
+  }
+});
 
 export default app;
