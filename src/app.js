1e2a81e8165ed85405a824586b058197637c8071
LucaPinheiro <lucapgomes11@gmail.com>
2023-10-11 17:17:19 -0300

    criando pasta para o middleware

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import conectaNaDatabase from "./config/dbConnect.js";
 import routes from "./routes/index.js";
 import mongoose from "mongoose";
+import manipuladorDeErros from "./middlewares/manipuladorDeErros.js";
 
 const conexao = await conectaNaDatabase();
 
@@ -15,14 +16,6 @@ app.use(express.json());
 routes(app);
 
 // eslint-disable-next-line no-unused-vars
-app.use((erro, req, res, next) => {
-  if (erro instanceof mongoose.Error.CastError) {
-    res
-      .status(400)
-      .send({ message: "Um ou mais dados fornecidos estão incorretos." });
-  } else {
-    res.status(500).send({ message: "Erro inteino de servidor" });
-  }
-});
+app.use(manipuladorDeErros);
 
 export default app;
