e43358b713757884139a2ef7e94b25f4c1a812c8
LucaPinheiro <lucapgomes11@gmail.com>
2023-11-08 09:24:53 -0300

    refatorando validação personalizada mongoose

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -16,13 +16,18 @@ const livroSchema = new mongoose.Schema({
     required: [true, "A editora é obrigatória"],
     enum: {
       values: ["Casa do código", "Editora de livros do Luca"],
-      message: "A editora {VALUE} fornecida não é um valor permitido",
+      message: "A editora  {VALUE} fornecida não é um valor permitido",
     },
   },
   numeroPaginas: {
     type: Number,
-    min: [10, "O número de páginas deve estar entre 10 e 5000"],
-    max: [5000, "O número de páginas deve estar entre 10 e 5000"],
+    validate: {
+      validator: (valor) => {
+        return valor >= 10 && valor <= 5000;
+      },
+      message:
+        "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}",
+    },
   },
 });
 
